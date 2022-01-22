3e4659c7b0ec533d8bf87684cce091e57c7d7777
BlueCannonBall <mmttiizz100@gmail.com>
2022-01-22 15:44:04 -0500

    proper spacing

--- polynet.hpp
+++ polynet.hpp
@@ -6,8 +6,10 @@
     #ifndef _WIN32_WINNT
         #define _WIN32_WINNT 0x0502 // Windows Server 2003
     #endif
+
     #include <winsock2.h>
     #include <ws2tcpip.h>
+
     #pragma comment(lib, "ws2_32.lib")
 #else
     #include <arpa/inet.h>
