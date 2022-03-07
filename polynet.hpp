7d29c0b0c3a44b6414a9451a403b9c9f303136d4
BlueCannonBall <mmttiizz100@gmail.com>
2022-03-07 11:28:43 -0500

    Update polynet.hpp

--- polynet.hpp
+++ polynet.hpp
@@ -18,6 +18,7 @@
     #include <netdb.h>
     #include <netinet/in.h>
     #include <netinet/ip.h>
+    #include <netinet/ip6.h>
     #include <netinet/tcp.h>
     #include <netinet/udp.h>
     #include <sys/socket.h>
