85cf8e54fe65b230ff84c9180cda7913b4edbd03
BlueCannonBall <mmttiizz100@gmail.com>
2022-08-11 21:08:07 -0400

    msc ver

--- polynet.hpp
+++ polynet.hpp
@@ -12,7 +12,9 @@
     #include <winsock2.h>
     #include <ws2tcpip.h>
 
-    #pragma comment(lib, "ws2_32.lib")
+    #ifdef _MSC_VER
+        #pragma comment(lib, "ws2_32.lib")
+    #endif
 #else
     #include <arpa/inet.h>
     #include <netdb.h>
